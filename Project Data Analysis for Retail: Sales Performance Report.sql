089cca60dc15deabd4ae06b7c2b26137f3208014
Alifia Harmadi <76767830+alifiaharmd@users.noreply.github.com>
2021-03-26 16:47:06 +1100

    Update Project Data Analysis for Retail: Sales Performance Report.sql

--- Project Data Analysis for Retail: Sales Performance Report.sql
+++ Project Data Analysis for Retail: Sales Performance Report.sql
@@ -17,6 +17,17 @@ where order_status = "Order Finished"
 group by years
 ;
 
+--Output
+
++-------+------------+-----------------+
+| years | sales      | number_of_order |
++-------+------------+-----------------+
+| 2009  | 4613872681 |            1244 |
+| 2010  | 4059100607 |            1248 |
+| 2011  | 4112036186 |            1178 |
+| 2012  | 4482983158 |            1254 |
++-------+------------+-----------------+
+
 --===============================================================================================
 /*1B. Overall performance of DQLab by product subcategory to be compared between 2011 and 2012*/
 
@@ -26,11 +37,39 @@ where order_status = "Order Finished" and (left(order_date,4) between "2011" and
 group by years, product_sub_category
 order by years, sales desc;
 
+--Output
+
++-------+------------------------------+-----------+
+| years | product_sub_category         | sales     |
++-------+------------------------------+-----------+
+|  2011 | Chairs & Chairmats           | 622962720 |
+|  2011 | Office Machines              | 545856280 |
+|  2011 | Tables                       | 505875008 |
+|  2011 | Copiers and Fax              | 404074080 |
+|  2011 | Telephones and Communication | 392194658 |
++-------+------------------------------+-----------+
+
 --==============================================================================================
 /*2A. The effectiveness and efficiency of promotions carried out so far, by calculating 
 the burn rate of the overall promotions by year*/
 
+select year(order_date) as years, 
+       sum(sales)as sales,
+       sum(discount_value) as promotion_Value,
+       round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
+from dqlab_sales_store
+where order_status = "Order Finished"
+group by year(order_date)
 
+--Output
++-------+------------+-----------------+----------------------+
+| years | sales      | promotion_value | burn_rate_percentage |
++-------+------------+-----------------+----------------------+
+|  2009 | 4613872681 |       214330327 |                 4.65 |
+|  2010 | 4059100607 |       197506939 |                 4.87 |
+|  2011 | 4112036186 |       214611556 |                 5.22 |
+|  2012 | 4482983158 |       225867642 |                 5.04 |
++-------+------------+-----------------+----------------------+
 
 --===============================================================================================
 /*2B. The effectiveness and efficiency of promotions carried out so far, by calculating 
