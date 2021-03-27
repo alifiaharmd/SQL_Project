f100e267476a58cc8272d5d866d891542bb51014
Alifia Harmadi <76767830+alifiaharmd@users.noreply.github.com>
2021-03-27 12:36:50 +1100

    Update Project Data Analysis for Retail: Sales Performance Report.sql

--- Project Data Analysis for Retail: Sales Performance Report.sql
+++ Project Data Analysis for Retail: Sales Performance Report.sql
@@ -75,10 +75,44 @@ group by year(order_date)
 /*2B. The effectiveness and efficiency of promotions carried out so far, by calculating 
 the burn rate of the overall promotions based on sub-categories*/
 
+select year(order_date) as years, 
+       product_sub_category, 
+       product_category, 
+       sum(sales) as sales, 
+       sum(discount_value) as promotion_Value, 
+       round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
+from dqlab_sales_store
+where order_status = "Order Finished" and year(order_date) ="2012"
+group by year(order_date),product_sub_category, product_category
+order by sales desc
 
+--Output
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+| years | product_sub_category           | product_category | sales     | promotion_Value | burn_rate_percentage |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
+|  2012 | Office Machines                | Technology       | 811427140 |        46616695 |                 5.75 |
+|  2012 | Chairs & Chairmats             | Furniture        | 654168740 |        26623882 |                 4.07 |
+|  2012 | Telephones and Communication   | Technology       | 422287514 |        18800188 |                 4.45 |
+|  2012 | Tables                         | Furniture        | 388993784 |        16348689 |                 4.20 |
+|  2012 | Binders and Binder Accessories | Office Supplies  | 363879200 |        22338980 |                 6.14 |
+|  2012 | Storage & Organization         | Office Supplies  | 356714140 |        18802166 |                 5.27 |
+|  2012 | Computer Peripherals           | Technology       | 308014340 |        15333293 |                 4.98 |
+|  2012 | Copiers and Fax                | Technology       | 292489800 |        14530870 |                 4.97 |
+|  2012 | Appliances                     | Office Supplies  | 266131100 |        14393300 |                 5.41 |
+|  2012 | Office Furnishings             | Furniture        | 178927480 |         8233849 |                 4.60 |
+|  2012 | Bookcases                      | Furniture        | 159984680 |        10024365 |                 6.27 |
+|  2012 | Paper                          | Office Supplies  | 126896160 |         6224694 |                 4.91 |
+|  2012 | Envelopes                      | Office Supplies  |  58629280 |         2334321 |                 3.98 |
+|  2012 | Pens & Art Supplies            | Office Supplies  |  43818480 |         2343501 |                 5.35 |
+|  2012 | Scissors, Rulers and Trimmers  | Office Supplies  |  36776400 |         2349280 |                 6.39 |
+|  2012 | Labels                         | Office Supplies  |  10007040 |          452245 |                 4.52 |
+|  2012 | Rubber Bands                   | Office Supplies  |   3837880 |          117324 |                 3.06 |
++-------+--------------------------------+------------------+-----------+-----------------+----------------------+
 
 --==============================================================================================
 --3A. Analysis of customers every year
+
+
 
 --==============================================================================================
 --3B. Analysis of the number of new customers each year
