9ced5cff5b3b963d9b7ea8eab8461cdba6ded7aa
Alifia Harmadi <76767830+alifiaharmd@users.noreply.github.com>
2021-03-27 15:48:04 +1100

    Create Data Analysis for E-commerce Challenge.sql

---
+++ Data Analysis for E-commerce Challenge.sql
@@ -0,0 +1,51 @@
+--======================================================================================
+-- A Challenge from DQLab
+
+--There were 4 data tables that used which were users, products, orders, order_details
+
+--users table
+
+
+
+--products table
+
+
+
+--orders table
+
+
+
+--order_details table
+
+
+--======================================================================================
+
+-- Data Exploration
+1. Products
+
+
+2. Orders
+
+
+3. Monthly Transaction 
+
+4. Transaction status
+
+5. User Transaction
+
+6. Top Buyer All Time
+
+7. Frequent Buyer
+
+8. Big Frequent Buyer 2020
+
+
+9. 
+
+-- Data Analysis Part A
+
+
+
+-- Data Analysis Part B
+
+
